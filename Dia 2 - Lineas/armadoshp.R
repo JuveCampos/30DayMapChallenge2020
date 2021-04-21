4e07bd6db6e8530009a9a298a1fc31d60489984d
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2021-04-21 17:56:35 -0500

    addGeojsonMetro

--- Dia 2 - Lineas/armadoshp.R
+++ Dia 2 - Lineas/armadoshp.R
@@ -1,9 +1,15 @@
 library(sf)
 library(tidyverse)
 a <- st_read("https://raw.githubusercontent.com/JuveCampos/Shapes_Resiliencia_CDMX_CIDE/master/geojsons/Division%20Politica/DivisionEstatal.geojson")
-cdmx <- a %>% 
+cdmx <- a %>%
   filter(ENTIDAD == "Ciudad de México")
 plot(cdmx,  max.plot = 1)
 
 st_write(cdmx, "/Users/admin/Desktop/Map Challenge 2020/cdmx_shape.geojson")
 st_write(cdmx, "/Users/admin/Desktop/Map Challenge 2020/cdmx_shape.shp")
+
+# Datos del metro ----
+met = st_read("metro85.shp")
+met$linea = 1:24
+
+write_sf(met, "metro85.geojson")
